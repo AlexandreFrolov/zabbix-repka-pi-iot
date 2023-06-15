c97874f6f60ef72f724fc39be0000480cb9d495e
Alexandre Frolov <alexandre@frolov.pp.ru>
2023-06-15 09:25:47 +0000

    Fix bh1750.py

--- bh1750.py
+++ bh1750.py
@@ -8,6 +8,6 @@ command = 0x10
 bus.write_byte(address, command)
 time.sleep(0.5)
 data = bus.read_i2c_block_data(address, command, 2)
-illuminance = (data[1] + (256 * data[0])) / 1.2
+illuminance = round((data[1] + (256 * data[0])) / 1.2)
 print(illuminance)
 
